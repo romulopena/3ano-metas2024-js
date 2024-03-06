bfa8d2f5bba67e9ee4eb3b3b3abb5b39e2220dee
PROF ROMULO <rjlpena@gmail.com>
2024-03-06 18:25:16 +0000

    Inserindo div contador no html abaixo do h4, inserindo const contadores e const tempoObjetivo1 e let tempoAtual e diminuindo o tempoObj do tempo atual

--- main.js
+++ main.js
@@ -14,4 +14,7 @@ for (let i= 0; i <botoes.length;i++){
     console.log();
 }
 
-//aula04
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-12-10T00:00:00");
+let tempoAtual = new Date();
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
